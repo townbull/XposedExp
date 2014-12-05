0f7e087b45bd718d066772758378dd59b6734f73
Bo Tang <botang.usa@gmail.com>
2014-12-05 13:08:44 -0800

    UI updated, saved the popup dialog for settings

--- src/com/samsung/xposedexp/MainActivity.java
+++ src/com/samsung/xposedexp/MainActivity.java
@@ -6,6 +6,7 @@ import com.samsung.xposedexp.LoadedPackage;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -19,45 +20,68 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends Activity {
-    private Switch swHttpBlock;
-    private Switch swLocationChange;
+
+    private SharedPreferences prefs;
+    private Switch prefHttp;
+    private Switch prefLocation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);        
-        
-//        swHttpBlock = (Switch) findViewById(R.id.swHttpBlock);
-//        swLocationChange = (Switch) findViewById(R.id.swLocationChange);
-//        		
-//        OnCheckedChangeListener	listner = new OnCheckedChangeListener() {       
-//			 @Override
-//			 public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-//			      if(isChecked){
-//			    	  Toast.makeText(getBaseContext(), buttonView.getText() + " is currently ON",
-//			    			   Toast.LENGTH_LONG).show();
-//			      }
-//			      else{
-//			    	  Toast.makeText(getBaseContext(), buttonView.getText() + " is currently OFF",
-//			   			   Toast.LENGTH_LONG).show();
-//			      }       
-//			 }
-//        };
-//        
-//        //set the switch to OFF 
-//        swHttpBlock.setChecked(false);
-//        swLocationChange.setChecked(false);
-//        //attach a listener to check for changes in state
-//        swHttpBlock.setOnCheckedChangeListener(listner);
-//        swLocationChange.setOnCheckedChangeListener(listner);
-         
-        Button changePrefs = (Button) findViewById(R.id.btn_prefs);
-        changePrefs.setOnClickListener(new OnClickListener() {
-            public void onClick(View v) {
-                Intent settingsActivity = new Intent(getBaseContext(), Preferences.class);
-                startActivity(settingsActivity);
+        setContentView(R.layout.activity_main);
+
+        // swHttpBlock = (Switch) findViewById(R.id.swHttpBlock);
+        // swLocationChange = (Switch) findViewById(R.id.swLocationChange);
+        //
+        // OnCheckedChangeListener listner = new OnCheckedChangeListener() {
+        // @Override
+        // public void onCheckedChanged(CompoundButton buttonView, boolean
+        // isChecked) {
+        // if(isChecked){
+        // Toast.makeText(getBaseContext(), buttonView.getText() +
+        // " is currently ON",
+        // Toast.LENGTH_LONG).show();
+        // }
+        // else{
+        // Toast.makeText(getBaseContext(), buttonView.getText() +
+        // " is currently OFF",
+        // Toast.LENGTH_LONG).show();
+        // }
+        // }
+        // };
+        //
+        // //set the switch to OFF
+        // swHttpBlock.setChecked(false);
+        // swLocationChange.setChecked(false);
+        // //attach a listener to check for changes in state
+        // swHttpBlock.setOnCheckedChangeListener(listner);
+        // swLocationChange.setOnCheckedChangeListener(listner);
+
+        prefs = getSharedPreferences("expSettings", Activity.MODE_WORLD_READABLE);
+
+        prefHttp = (Switch) findViewById(R.id.swHttpBlock);
+        prefHttp.setChecked(prefs.getBoolean("exp_block_http", false));
+
+        prefLocation = (Switch) findViewById(R.id.swLocationChange);
+        prefLocation.setChecked(prefs.getBoolean("exp_change_location", false));
+
+        Button mSave = (Button) findViewById(R.id.save);
+        mSave.setOnClickListener(new OnClickListener()
+        {
+            public void onClick(View v)
+            {
+                savePreferences();
+                Toast.makeText(getBaseContext(), "Settings saved successfully!", Toast.LENGTH_LONG).show();
             }
         });
+    }
+
+    private void savePreferences() {
+        prefs = getSharedPreferences("expSettings", Activity.MODE_WORLD_READABLE);
+        SharedPreferences.Editor editor = prefs.edit();
+        editor.putBoolean("exp_block_http", prefHttp.isChecked());
+        editor.putBoolean("exp_change_location", prefLocation.isChecked());
+        editor.commit();
     }
 
     @Override
@@ -73,9 +97,9 @@ public class MainActivity extends Activity {
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-//        if (id == R.id.action_settings) {
-//            return true;
-//        }
+        // if (id == R.id.action_settings) {
+        // return true;
+        // }
         return super.onOptionsItemSelected(item);
     }
 }
